f3f348298d335276d40d3d2b30d79207362254e0
Shanate <redeyes5240@gmail.com>
2024-06-25 11:47:06 +0900

    test

---
+++ algo/src/m0625/Main_bj_10810_공넣기_신재혁.java
@@ -0,0 +1,15 @@
+package m0625;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_10810_공넣기_신재혁 {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+
+	}
+
+}
