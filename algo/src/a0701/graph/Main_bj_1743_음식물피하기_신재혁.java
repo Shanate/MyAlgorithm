379dc53e0c720fabda298942b79c6a074466cad9
Shanate <redeyes5240@gmail.com>
2024-07-02 09:14:08 +0900

    Baekjoon

--- algo/src/a0701/graph/Main_bj_1743_음식물피하기_신재혁.java
+++ algo/src/a0701/graph/Main_bj_1743_음식물피하기_신재혁.java
@@ -4,43 +4,56 @@ import java.io.*;
 import java.util.*;
 
 public class Main_bj_1743_음식물피하기_신재혁 {
-	static int N,M,K;
-	static int[][] g;
-	static boolean[][] v;
-	static int Max;
-	
-	static void dfs(int i, int j) {
-		v[i][j]=true;
-		for(int k=0; k<N; k++) {
-			for(int l=0; l<N; l++) {
-			if(g[i][j] != 0 && !v[j]) {
-				dfs(j);
-			}
-		}
-	}
-	
-	public static void main(String[] args) throws Exception {
+    static int N,M,K;
+    static int[][] g;
+    static boolean[][] v;
+    static int Max;
+    static int cnt;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-			N = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
-			K = Integer.parseInt(st.nextToken());
-			g = new int[N][N];
-			v = new boolean[N][N];
-		
-		for(int i=0; i<K; i++) {
-			st = new StringTokenizer(br.readLine()," ");
-			int r = Integer.parseInt(st.nextToken()) - 1; // 시정점
-			int c = Integer.parseInt(st.nextToken()) - 1; // 종정점
-			g[r][c]=1;
-		}
-		
-		dfs(0,0);
-		System.out.println(Max);
-		
-	}
+    static int[] di = {-1, 0, 1, 0};
+    static int[] dj = {0, 1, 0, -1};
 
+    static void dfs(int i, int j) {
+        v[i][j] = true;
+        cnt++;
+
+        for (int d = 0; d < 4; d++) {
+            int ni = i + di[d];
+            int nj = j + dj[d];
+
+            if (ni >= 0 && ni < N && nj >= 0 && nj < M && g[ni][nj] != 0 && !v[ni][nj]) {
+                dfs(ni, nj);
+            }
+        }
+    }
+        public static void main(String[] args) throws Exception {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            g = new int[N][M];
+            v = new boolean[N][M];
+            cnt = 0;
+            Max = 0;
+
+            for(int i=0; i<K; i++) {
+                st = new StringTokenizer(br.readLine()," ");
+                int r = Integer.parseInt(st.nextToken()) - 1; // 시정점
+                int c = Integer.parseInt(st.nextToken()) - 1; // 종정점
+                g[r][c]=1;
+            }
+
+            for(int i=0; i<N; i++){
+                for(int j=0; j<M;j++){
+                    if(g[i][j] != 0 && !v[i][j]){
+                        cnt = 0;
+                        dfs(i,j);
+                        Max = Math.max(Max, cnt);
+                    }
+                }
+            }
+            System.out.println(Max);
+        }
 }
-
-// N : 세로	M : 가로		K : 쓰레기 개수(정점) 	k개 : (r, c) 음식물이 떨어진 좌표	r은 위에서부터, c는 왼쪽에서부터
